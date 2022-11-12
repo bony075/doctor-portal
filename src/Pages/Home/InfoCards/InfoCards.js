a7dd5fb3ba5782094e5c3d5f5e95dfb5ca42f179
bony075 <bony.075@gmail.com>
2022-11-12 17:55:50 +0600

    info card section added

--- src/Pages/Home/InfoCards/InfoCards.js
+++ src/Pages/Home/InfoCards/InfoCards.js
@@ -2,6 +2,7 @@ import React from 'react';
 import clock from '../../../assets/icons/clock.svg'
 import marker from '../../../assets/icons/marker.svg'
 import phone from '../../../assets/icons/phone.svg'
+import InfoCard from './InfoCard';
 
 const InfoCards = () => {
     const cardData = [
@@ -11,11 +12,32 @@ const InfoCards = () => {
             description: 'Open 9.00 am to 5.00 pm everyday',
             icon: clock,
             bgClass:'bg-primary'
-        }
+        },
+        {
+            id: 2,
+            name: 'Our Location',
+            description: 'Open 9.00 am to 5.00 pm everyday',
+            icon: marker,
+            bgClass:'bg-accent'
+        },
+        {
+            id: 3,
+            name: 'Contact Us',
+            description: 'Open 9.00 am to 5.00 pm everyday',
+            icon: phone,
+            bgClass:'bg-primary'
+        },
     ]
     return (
-        <div>
-            
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {
+                cardData.map(cd => <InfoCard
+                
+                    key = { cd.id }
+                    card={cd}
+                
+                ></InfoCard>)
+            }
         </div>
     );
 };
